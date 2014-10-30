ce7c7276b17f20edb4d8e1faa6f433f06305a7b4
= <=>
2014-10-29 18:13:29 -0700

    Revert "Add Auth project"
    This reverts commit 95b91b4f3f2fa4f24a2d02b5dd3b8faa98ff84a4.

--- v2/db/migrate/20141029203556_add_user_to_cat_rental_requests.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToCatRentalRequests < ActiveRecord::Migration
-  def change
-    add_column :cat_rental_requests, :user_id, :integer
-    change_column :cat_rental_requests, :user_id, :integer, null: false
-  end
-end
--- v2/db/migrate/20141029224749_update_user.rb
+++
@@ -1,5 +0,0 @@
-class UpdateUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :session_token
-  end
-end
